4f083848f3bc8de3a30baa940bb6a63848aea002
Anas Abu Nahel <125291685+AnasAbuNahel@users.noreply.github.com>
2025-06-02 13:30:18 +0300

    Update ResidentAidModal.js

--- frontend/al-furqan-frontend/src/components/ResidentAidModal.js
+++ frontend/al-furqan-frontend/src/components/ResidentAidModal.js
@@ -5,7 +5,7 @@ export default function ResidentAidModal({ residentId, onClose }) {
   const [resident, setResident] = useState(null);
 
   useEffect(() => {
-    axios.get(`http://127.0.0.1:5000/resident/${residentId}`)
+    axios.get(`https://al-furqan-project-82pm.onrender.com/resident/${residentId}`)
       .then(res => setResident(res.data))
       .catch(err => console.error('خطأ في تحميل بيانات الساكن:', err));
   }, [residentId]);
