8fa6b45c8305237a314b5d4916ee8d00fce756e2
Anas Abu Nahel <125291685+AnasAbuNahel@users.noreply.github.com>
2025-06-02 13:06:48 +0300

    Add files via upload

---
+++ frontend/al-furqan-frontend/src/api.js
@@ -0,0 +1,9 @@
+const API_BASE_URL =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:5000"
+    : "https://al-furqan-project-82pm.onrender.com";
+
+export default API_BASE_URL;
+
+
+
