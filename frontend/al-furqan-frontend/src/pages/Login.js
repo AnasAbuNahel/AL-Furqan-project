d57c433e07328115bd52fa3268262e2b5d2827c8
Anas Abu Nahel <125291685+AnasAbuNahel@users.noreply.github.com>
2025-06-15 00:50:55 +0300

    Update Login.js

--- frontend/al-furqan-frontend/src/pages/Login.js
+++ frontend/al-furqan-frontend/src/pages/Login.js
@@ -35,7 +35,7 @@ const Login = () => {
     setLoading(true);
 
     try {
-      const response = await axios.post('http://127.0.0.1:5000/api/login', {
+      const response = await axios.post('https://al-furqan-project-uqs4.onrender.com/api/login', {
         username,
         password,
       });
