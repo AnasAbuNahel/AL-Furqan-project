63be2df732cc453d473dcc2cac73fc7349114120
Anas Abu Nahel <125291685+AnasAbuNahel@users.noreply.github.com>
2025-06-15 00:38:52 +0300

    Update Settings.js

--- frontend/al-furqan-frontend/src/pages/Settings.js
+++ frontend/al-furqan-frontend/src/pages/Settings.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { toast } from 'react-hot-toast';
-import { saveAdmins, getLocalAdmins } from '../db';
 
 const Settings = () => {
   const [admins, setAdmins] = useState([]);
