5b04f059ca4a3f7fc59fc5445bd766d195ede2a4
Anas Abu Nahel <125291685+AnasAbuNahel@users.noreply.github.com>
2025-06-15 00:50:28 +0300

    Update Dashboard.js

--- frontend/al-furqan-frontend/src/pages/Dashboard.js
+++ frontend/al-furqan-frontend/src/pages/Dashboard.js
@@ -5,7 +5,7 @@ import toast, { Toaster } from "react-hot-toast";
 import axios from "axios";
 
 // إعداد القاعدة العامة لـ Axios
-axios.defaults.baseURL = "http://localhost:5000";
+axios.defaults.baseURL = "https://al-furqan-project-uqs4.onrender.com";
 axios.defaults.headers.common["Authorization"] = `Bearer ${localStorage.getItem("token")}`;
 
 const Dashboard = () => {
