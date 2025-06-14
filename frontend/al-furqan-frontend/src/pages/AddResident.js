6f9dab5c41941c3ed73552b3e5e0510549b9b33e
Anas Abu Nahel <125291685+AnasAbuNahel@users.noreply.github.com>
2025-06-15 00:32:18 +0300

    Update AddResident.js

--- frontend/al-furqan-frontend/src/pages/AddResident.js
+++ frontend/al-furqan-frontend/src/pages/AddResident.js
@@ -3,7 +3,6 @@ import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { saveResidentOffline } from '../utils/idb';
 
 const AddResident = () => {
   const navigate = useNavigate();
