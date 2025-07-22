1a0c81df4cca9af3fc6675090a803bc01bc6deb5
Anas Abu Nahel <125291685+AnasAbuNahel@users.noreply.github.com>
2025-07-22 21:31:29 +0300

    Update App.js

--- frontend/al-furqan-frontend/src/App.js
+++ frontend/al-furqan-frontend/src/App.js
@@ -6,6 +6,7 @@ import AddResident from './pages/AddResident';
 import ResidentsList from './pages/ResidentsList';
 import AidForm from './pages/AidForm';
 import AidHistory from './pages/AidHistory';
+import AidHistory from './pages/ChildrenRecord';
 import Statistics from './pages/Statistics';
 import Settings from './pages/Settings';
 import Dashboard from './pages/Dashboard';
@@ -28,6 +29,7 @@ function App() {
           <Route path="/residents" element={<PrivateRoute><ResidentsList /></PrivateRoute>} />
           <Route path="/aid" element={<PrivateRoute><AidForm /></PrivateRoute>} />
           <Route path="/history" element={<PrivateRoute><AidHistory /></PrivateRoute>} />
+          <Route path="/Child" element={<PrivateRoute><ChildrenRecord /></PrivateRoute>} />
           <Route path="/stats" element={<PrivateRoute><Statistics /></PrivateRoute>} />
           <Route path="/dash" element={<PrivateRoute><Dashboard /></PrivateRoute>} />
           <Route path="/settings" element={<PrivateRoute><Settings /><Toaster position="top-center" reverseOrder={false} /></PrivateRoute>} />
